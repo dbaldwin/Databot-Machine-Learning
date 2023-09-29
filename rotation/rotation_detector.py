b11343960885d3a87603790c3663789e6aeba355
Patrick Ryan <theyoungsoul@gmail.com>
2023-09-29 07:49:19 -0500

    updates for the rotation project

--- rotation/rotation_detector.py
+++ rotation/rotation_detector.py
@@ -1,40 +1,31 @@
-import sys
-sys.path.append("../..")
+import logging
 
-from databot.PyDatabot import PyDatabot, DatabotConfig, DatabotBLEConfig
-from joblib import dump, load
 import pandas as pd
+from databot.PyDatabot import PyDatabot, DatabotConfig
+from joblib import load
+
 
 class PyDatabotRotationDetector(PyDatabot):
+    def __init__(self, databot_config: DatabotConfig, model_file_name: str, log_level: int = logging.INFO):
+        super().__init__(databot_config, log_level)
+        self.model = load(model_file_name)
+        self.labels = ['steady', 'pendulum', 'vertical', 'horizontal']  # 0-steady, 1-pendulum, 2-vertical, 3-horizontal
 
-    async def run_queue_consumer(self):
-        self.logger.info("Starting rotation detector")
-        rotational_model = load('./rotation_detector_model.sav')
-        labels = ['steady', 'pendulum', 'vertical', 'horizontal'] # 0-steady, 1-pendulum, 2-vertical, 3-horizontal
-        while True:
-            # Use await asyncio.wait_for(queue.get(), timeout=1.0) if you want a timeout for getting data.
-            epoch, data = await self.queue.get()
-            if data is None:
-                self.logger.info(
-                    "Got message from client about disconnection. Exiting consumer loop..."
-                )
-                break
-            else:
-                print(data)
-                data_record = pd.DataFrame([data])
-                data_record = data_record.drop(columns=['time'])
-                prediction_proba = rotational_model.predict_proba(data_record)
-                prediction = rotational_model.predict(data_record)
-                self.logger.info(f"Rotation Prediction: {prediction[0]} ({labels[prediction[0]]}), {prediction_proba}")
+    def process_databot_data(self, epoch, data):
+        df = pd.DataFrame([data])
+        df.drop(columns=['time'], inplace=True)
+        prediction = self.model.predict(df)
+        print(f"{data['time']}: {prediction} ({self.labels[prediction[0]]})")
 
 
 def main():
     c = DatabotConfig()
-    c.gyro = True
+    c.gyro = False
     c.Laccl = True
+    c.refresh = 200
     c.address = PyDatabot.get_databot_address()
 
-    db = PyDatabotRotationDetector(c)
+    db = PyDatabotRotationDetector(databot_config=c, model_file_name="rotation_detector_model_no_horizontal.sav")
     db.run()
 
 
--- rotation/rotational_data_collector.py
+++ rotation/rotational_data_collector.py
@@ -3,14 +3,14 @@ from databot.PyDatabot import PyDatabot, PyDatabotSaveToFileDataCollector, Datab
 
 def main(filename, target_value):
     c = DatabotConfig()
-    c.gyro = True
+    c.gyro = False
     c.Laccl = True
     c.refresh = 200
     c.address = PyDatabot.get_databot_address()
     db = PyDatabotSaveToFileDataCollector(c,
                                           file_name=f"data/{filename}",
                                           number_of_records_to_collect=1000,
-                                          # 0-steady, 1-pendulum, 2-horizontal, 3-vertical
+                                          # 0-steady, 1-pendulum, 2-vertical, 3-horizontal
                                           extra_data={
                                               "rotation": target_value
                                           }
@@ -23,4 +23,4 @@ if __name__ == '__main__':
     # main("steady.txt", 0)
     # main("pendulum.txt", 1)
     # main("vertical.txt", 2)
-    # main("horizontal.txt", 3)
+    main("horizontal.txt", 3)
